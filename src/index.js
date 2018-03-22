40338de183c89d937e01bdc9ac6de353adc736ca
Yuliia Shamatiienko <y@ione.software>
2018-03-22 14:17:00 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const css = require('./main.scss');
+
+console.log("I'm a silly entry point!!!");
+console.log('Hello!');
---
+++ webpack.config.js
@@ -0,0 +1,126 @@
+const path = require("path");
+const glob = require('glob');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+const bootstrapEntryPoints = require('./webpack.bootstrap.config');
+const PurifyCSSPlugin = require('purifycss-webpack');
+
+const SRC_DIR = path.resolve(__dirname, "src");
+const BUILD_DIR = path.resolve(__dirname, "dist");
+
+const isProd = process.env.NODE_ENV === 'production'; //true or false
+const cssDev = ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'];
+const cssProd = ExtractTextPlugin.extract({
+  fallback: 'style-loader',
+  use: ['css-loader', 'sass-loader', 'postcss-loader'],
+  publicPath: BUILD_DIR
+});
+
+const cssConfig = isProd ? cssProd : cssDev;
+const bootstrapConfig = isProd ? bootstrapEntryPoints.prod : bootstrapEntryPoints.dev;
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+    bootstrap: bootstrapConfig
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: "[name].bundle.js",
+    publicPath: ""
+  },
+  // devtool: "sourcemap",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: cssConfig
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          // 'file-loader?name=img/[hash:6].[ext]',
+          'file-loader?name=[hash:6].[ext]&outputPath=img/&publicPath=img/',
+          'image-webpack-loader'
+        ]
+        // use: 'file-loader?name=[name].[ext]&outputPath=img/',
+      },
+      {
+        test: /\.(woff2?|svg)$/, loader: 'url-loader?limit=10000&name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.(ttf|eot)$/, loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      // {
+      //   test: /\.js$/,
+      //   use: ["source-map-loader"],
+      //   enforce: "pre"
+      // },
+      { test: /bootstrap[\/\\]dist[\/\\]js[\/\\]umd[\/\\]/, loader: 'imports-loader?jQuery=jquery' },
+      {
+        test: /font-awesome\.config\.js/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'font-awesome-loader' }
+        ]
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/dist"),
+    compress: true,
+    hot: true,
+    port: 9000,
+    stats: 'errors-only'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: "./src/index.html",
+        to: "index.html"
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: "./src/index.html"
+    }),
+    new ExtractTextPlugin({
+      filename: '/css/[name].css',
+      disable: !isProd,
+      allChunks: true
+    }),
+    // Make sure this is after ExtractTextPlugin!
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(path.join(__dirname, 'src/*.html')),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      tether: 'tether',
+      Tether: 'tether',
+      'window.Tether': 'tether',
+      Popper: ['popper.js', 'default'],
+      'window.Tether': 'tether',
+      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+      Button: 'exports-loader?Button!bootstrap/js/dist/button',
+      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Util: 'exports-loader?Util!bootstrap/js/dist/util'
+    }),
+  ]
+};
